ab2a90ce335cbe84bf2e5ccfb032d1e8b887dbea
SergejsRom <sergejsromanovskis@gmail.com>
2022-03-30 22:10:27 +0300

    30-03

---
+++ darbai/30-03.js
@@ -0,0 +1,29 @@
+// 1 uzduotis //
+const n1 = 20
+
+if (n1 > 0) {
+    console.log('Skaičius geras.');
+} else {
+    console.log('Skaičius blogas');
+   }
+
+// 2 uzduotis //
+const n2 = 2;
+if (n2 === 2) {
+    console.log('Palaukite')
+} 
+
+//3 uzduotis //
+const d = 3;
+const k = 18;
+const n3 = 5;
+
+if (k / n3 <= d) {
+    console.log('Knygos telpa į dėžes');
+} else {
+    console.log('Knygos netelpa į dėžes.');
+   }
+
+// 4 uzduotis //
+const a = 10;
+const b = 2;
