29abc3d66b67d290e321ebf129caa00145a60a37
SergejsRom <sergejsromanovskis@gmail.com>
2022-04-04 23:05:45 +0300

    edited

--- darbai/04-01-for.js
+++ darbai/04-01-for.js
@@ -9,13 +9,18 @@ for (let i = 0; i < 5; i++) {
     result +=i;
     console.log(zodis);
 }
+const sveikas = 'sveikas'
+for (let i = 0; i < 5; i++) {
+    console.log(sveikas)};
 // 2 uzd //
 console.log('-----------------');
 for (let i = 0; i < 5; i++) {
     let result = 0;
     result +=i
     console.log(result);
-    
+}
+for (let i = 0; i < 5; i++) {
+    console.log(i);
 }
 // 3 uzd //
 console.log('-----------------');
@@ -24,8 +29,12 @@ for (let i = 0; i < 5; i++) {
     let resultatas = 0;
     resultatas +=i*10;
     console.log(resultatas);
-    
 }
+
+for (let i = 0; i < 50; i+=10) {
+    console.log(i);
+}
+
 
 // 4 uzd //
 console.log('-----------------');
@@ -35,6 +44,9 @@ for (let i = 49; i <= 53; i++) {
     resultatas +=i;
     console.log(resultatas);
 }
+for (let i = 49; i <= 53; i++) {
+    console.log(i)
+}
 
 // 5 uzd //
 console.log('-----------------');
@@ -42,6 +54,10 @@ for (let i = 0; i <5; i++) {
     let result = 0;
     result = (Math.floor(Math.random() * (10 - 0 + 1)) + 0) ; // (Math.floor(Math.random() * (max - min + 1)) + min //
     console.log(result)
+}
+for (let i = 0; i <5; i++) {
+    let i = (Math.floor(Math.random() * (10 - 0 + 1)) + 0) ; // (Math.floor(Math.random() * (max - min + 1)) + min //
+    console.log(i)
 }
 
 // 6 uzd //
@@ -61,6 +77,10 @@ for (let a = 1; a <= 10; a++) {
       //console.log(a + "*" + f + "=" +(a*f))
 }
 }
+for (let i = 1; i <= 10; i++) {
+    const skaicius = 5;
+    console.log(`${i} * ${skaicius} = ${i * skaicius}`)
+}
 
 // 8 uzd //
 const n = 10
@@ -78,23 +98,15 @@ const indelis = 100;
 const nMetai = 8;
 
 for (let i = 1; i <= nMetai; i++) {
-    for (let a = palukanos; a < nMetai; a++) {
-        for (let b = indelis; b < nMetai; b++) {
-               
-        }
-        
-    }
-        let met = +i;
-        pal = palukanos * indelis;
-        ind = indelis + pal
-        
-        console.log(`metai ${met}, palukanos ${pal}, indelis ${ind}` );
+   let pal = 0;
+   pal = (indelis * palukanos);
+        console.log(`metai: ${i}, palukanos: ${pal}, indelis: ${indelis + pal[i]} ` );
 }
 
 // 10 uzd //
 console.log('-----------------');
 
-for (let i = 1; i <= 2022; i++) {
+for (let i = 1; i <= 20; i++) {
     { if (i % 4 === 0) 
             console.log(`keliamieji metai: ${i}`);
         }
