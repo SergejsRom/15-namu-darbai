be533cf8653eedcf4c88c130634735cffe00991f
SergejsRom <sergejsromanovskis@gmail.com>
2022-04-07 02:05:44 +0300

    while without 3

---
+++ darbai/04-06-while.js
@@ -0,0 +1,51 @@
+console.log('---------1 uzd---------')
+
+let suma1 = 0;
+while (suma1 <= 99) {
+  const skaicius = (Math.floor(Math.random() * (10 - 1 + 1)) + 1)
+  console.log(skaicius)
+  suma1 += skaicius;
+ 
+}
+console.log(suma1);
+
+console.log('---------2 uzd---------')
+let divide7 = 0;
+while (isFinite) {
+  const skaicius = (Math.floor(Math.random() * (10 - 1 + 1)) + 1);
+  console.log(skaicius)
+  divide7 += skaicius;
+  if (divide7 % 7 === 0) {
+    break;
+  }
+}
+console.log(divide7)
+
+
+console.log('---------3 uzd---------')
+
+
+
+
+console.log('---------4 uzd---------')
+let number5 = 0
+while (isFinite) {
+  number5 = (Math.floor(Math.random() * (10 - 1 + 1)) + 1);
+  console.log(number5)
+  if (number5 === 5) {
+    break;
+  }
+}
+console.log(number5)
+
+console.log('---------5 uzd---------')
+let number57 = 0
+while (isFinite) {
+  number57 = (Math.floor(Math.random() * (10 - 1 + 1)) + 1);
+  console.log(number57)
+  if (number57 === 5 || number57 === 7) {
+    break;
+  }
+}
+console.log(number57)
+
