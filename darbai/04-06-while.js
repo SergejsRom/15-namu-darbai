c1b60b51ad8a1338313eafa9f2971dbf421a8fd8
SergejsRom <sergejsromanovskis@gmail.com>
2022-04-07 20:47:41 +0300

    visos uzduotis

--- darbai/04-06-while.js
+++ darbai/04-06-while.js
@@ -10,45 +10,45 @@ while (suma1 <= 99) {
 console.log(suma1);
 
 console.log('---------2 uzd---------')
+
 let divide7 = 0;
-while (isFinite) {
-  const skaicius = (Math.floor(Math.random() * (10 - 1 + 1)) + 1);
-  console.log(skaicius)
+
+while (divide7 % 7 !== 0 || divide7 === 0) {
+   let skaicius = (Math.floor(Math.random() * (10 - 1 + 1)) + 1);
   divide7 += skaicius;
-  if (divide7 % 7 === 0) {
-    break;
-  }
+  console.log(skaicius) 
 }
-console.log(divide7)
+console.log(divide7, 'dalijasi is 7')
 
 
 console.log('---------3 uzd---------')
-
-
-
+const skyriuViso = 8;
+let skyriaiPerDiena = 0;
+let kiekDienu = 0;
+while (skyriuViso > skyriaiPerDiena) {
+  kiekDienu++;
+  skyriaiPerDiena += kiekDienu
+}
+vidutiniskai = skyriuViso / kiekDienu
+console.log(`perskaitys per ${kiekDienu} dienas (-ų), vidutiniškai per dieną perskaitė ${vidutiniskai.toFixed()}`);
 
 console.log('---------4 uzd---------')
 let number5 = 0;
-let ratai = 0;
-while (isFinite) {
+while (number5 !==5) {
   number5 = (Math.floor(Math.random() * (10 - 1 + 1)) + 1);
   console.log(number5)
-  if (number5 === 5) {
-    break;
-  }
+  
 }
 console.log('paskutinis', number5)
 
 console.log('---------5 uzd---------')
-let number57 = 0;
+let number57;
 let ciklas = 0;
-while (isFinite) {
+while (number57 !==5 && number57 !==7 ) {
   number57 = (Math.floor(Math.random() * (10 - 1 + 1)) + 1);
   console.log(number57);
   ciklas++
-  if (number57 === 5 || number57 === 7) {
-    break;
-  }
+  
 }
 console.log(`prasisuko ${ciklas} ciklu, paskutinis skaicius ${number57}`)
 
