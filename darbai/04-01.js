fa4ddc4f50d6ec564bd5ce11187649575d9bb01d
SergejsRom <sergejsromanovskis@gmail.com>
2022-04-11 00:49:40 +0300

    masyvai

--- darbai/04-01.js
+++ darbai/04-01.js
@@ -22,8 +22,7 @@ function palyginti(a, b) {
     console.log(rezultatas);
 
     //ARBA
-    const pirmieji = [3, 3];
-    const antrieji = [5 ,7];
+
     const tretieji = [8, 4];
     function palyginimas (skaiciai) {
         if (skaiciai[0] > skaiciai[1]) { 
@@ -33,17 +32,12 @@ function palyginti(a, b) {
         } if (skaiciai[0] === skaiciai[1]) {
             return 'lygus'
         }}
-        const result1 = palyginimas(pirmieji)
-        console.log(result1);
-        const result2 = palyginimas(antrieji)
-        console.log(result2);
         const result3 = palyginimas(tretieji)
         console.log(result3);
 
         // 3 uzd //
         console.log('-----------------');
-const metai1 = 2019;
-const metai2 = 1856;
+
 const metai3 = 789;
 const metai4 = 3256;
 function keliamieji(pirmi) {
@@ -52,10 +46,7 @@ function keliamieji(pirmi) {
     } else {
         return 'ne'
 }}
-const arKeliamieji1 = keliamieji(metai1);
-console.log(arKeliamieji1);
-const arKeliamieji2 = keliamieji(metai2);
-console.log(arKeliamieji2);
+
 const arKeliamieji3 = keliamieji(metai3);
 console.log(arKeliamieji3);
 const arKeliamieji4 = keliamieji(metai4);
@@ -125,13 +116,12 @@ console.log(kiek2);
 console.log('-----------------');
 const number1 = [8, 6, 8, 4, 9, 2, 5, 7, 5, 5];
 const number2 = [5, 4, 3, 2, 1, 0, 6, 7, 8, 9];
-function phone(n) {
-    return `(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}`
+function telefonoNumeris(n) {
+    return `(${n[1]}${n[1]}${n[1]}) ${n[2]}${n[2]}${n[2]}-${n[3]}${n[3]}${n[3]}${n[3]}`
 }
-const phoneFormat1 = phone(number1);
-console.log(phoneFormat1);
-const phoneFormat2 = phone(number2);
-console.log(phoneFormat2);
+const pakeistas = telefonoNumeris(number1);
+console.log(pakeistas);
+
 
 // 9uzd //
 console.log('-----------------');
---
+++ darbai/04-10-masyvai.js
@@ -0,0 +1,21 @@
+//1.Sukurti masyą, kurio ilgis 20, o reikšmės –skaičiai, nuo 1 iki 20.
+console.log(' 1 uzd ');
+
+let a = []
+for (let i = 1; i < 21; i++) {
+    i+
+    a.push(i);   
+}
+console.log(a);
+console.log('-------------');
+
+//2.Sukurti atsitiktinio ilgio masyvą, nuo 40 iki 60, kurio reikšmės būtų skaičiai nuo 1 iki masyvo ilgio.
+console.log(' 2 uzd ');
+
+//3.Sukurti atsitiktinio ilgio masyva, kurio ilgisnuo 10iki 15, o reikšmės –atsitiktiniai skaičiai nuo 5 iki 47.Surasti didžiausia skaičių masyve, nenaudojant sort ir Math.max funkcijų.
+
+//4.Sukurti masyvą, kurio ilgis 100, o reikšmės –raidės D, E, F, G, H. 20. 
+
+//5.Sukurti masyvą, kurio ilgis 100, o reikšmės –raidės M,N,O,P. Suskaičiuoti, kurios raidės buvo daugiausia.
+
+//6.Sugeneruotimasyvą, kurio ilgis 20, o reikšmės masyve turi būti nuo 10 iki 50. Reikšmės masyve turi būti unikalios, tai yra –nesikartoti.
